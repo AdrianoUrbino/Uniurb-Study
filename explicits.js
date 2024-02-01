3aaeea846cab015736bce9acd9d6716feb235483
AdrianoUrbino <157110369+AdrianoUrbino@users.noreply.github.com>
2024-02-01 16:19:46 +0100

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -45,17 +45,13 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
         answers: [
-            {text:'10 - Extremely warm', value:10},
-            {text:'9 - Very warm', value:9},
-            {text:'8 - Moderately warm', value:8},
-            {text:'7 - Somewhat warm', value:7},
-            {text:'6 - Slightly warm', value:6},
-            {text:'5 - Neither warm nor cold', value:5},
-            {text:'4 - Slightly cold', value:4},
-            {text:'3 - Somewhat cold', value:3},
-            {text:'2 - Moderately cold', value:2},
-            {text:'1 - Very cold', value:1},
-            {text:'0 - Extremely cold', value:0}
+           
+            {text:'1', value:1},
+            {text:'2', value:2},
+            {text:'3', value:3},
+            {text:'4', value:4},
+            {text:'5', value:5},
+            
         ]
     });
 
@@ -81,15 +77,93 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
         name: 'Tblack_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
+        stem: 'Stiamo raggiungendo il limite massimo del numero di persone su questa Terra'
     });
 
     API.addQuestionsSet('thermWhite',{
         inherit : 'therm',
         name: 'Twhite_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
+        stem: 'Gli esseri umani hanno il diritto di modificare l’ambiente naturale per i propri bisogni'
     });
 
+    API.addQuestionsSet('Item3',{
+        inherit : 'therm',
+        name: 'Item3',
+        stem: 'Quando gli esseri umani interferiscono con la natura, si producono effetti disastrosi'
+    });
+
+    API.addQuestionsSet('Item4',{
+        inherit : 'therm',
+        name: 'Item4',
+        stem: 'Grazie all’ingegno umano, la Terra rimarrà un luogo vivibile'
+    });
+
+    API.addQuestionsSet('Item5',{
+        inherit : 'therm',
+        name: 'Item5',
+        stem: 'Gli esseri umani stanno abusando gravemente dell’ambiente'
+    });
+
+    API.addQuestionsSet('Item6',{
+        inherit : 'therm',
+        name: 'Item6',
+        stem: 'La Terra in realtà ha tante risorse naturali se solo sapessimo farne buon uso'
+    });
+
+    API.addQuestionsSet('Item7',{
+        inherit : 'therm',
+        name: 'Item7',
+        stem: 'Gli esseri umani hanno il dovere di tutelare la vita di animali e piante'
+    });
+
+    API.addQuestionsSet('Item8',{
+        inherit : 'therm',
+        name: 'Item8',
+        stem: 'L’equilibrio dell’ambiente è forte abbastanza da reggere l’impatto delle società industrializzate'
+    });
+
+    API.addQuestionsSet('Item9',{
+        inherit : 'therm',
+        name: 'Item9',
+        stem: 'Malgrado i progressi, siamo ancora in balia della forza della natura'
+    });
+
+    API.addQuestionsSet('Item10',{
+        inherit : 'therm',
+        name: 'Item10',
+        stem: 'I problemi ambientali sono stati in larga misura esagerati'
+    });
+
+    API.addQuestionsSet('Item11',{
+        inherit : 'therm',
+        name: 'Item11',
+        stem: 'La Terra ha risorse limitate'
+    });
+
+    API.addQuestionsSet('Item12',{
+        inherit : 'therm',
+        name: 'Item12',
+        stem: 'Gli esseri umani sono destinati a comandare sulla natura'
+    });
+
+    API.addQuestionsSet('Item13',{
+        inherit : 'therm',
+        name: 'Item13',
+        stem: 'L’equilibrio della natura è delicato e fragile'
+    });
+
+    API.addQuestionsSet('Item14',{
+        inherit : 'therm',
+        name: 'Item14',
+        stem: 'Con il tempo gli esseri umani impareranno come funziona la natura e riusciranno a controllarla'
+    });
+
+    API.addQuestionsSet('Item15',{
+        inherit : 'therm',
+        name: 'Item15',
+        stem: 'Se le cose vanno avanti così, presto ci sarà una catastrofe ambientale'
+    });
+	
     API.addSequence([
         {
             mixer : 'random', 
